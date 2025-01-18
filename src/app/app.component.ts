8a4d7f60530ccf44a65c3cbe703965dac69a7377
Mo-Zoka <zoka1080p@gmail.com>
2025-01-18 20:28:56 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BodyComponent } from './components/body/body.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FontAwesomeModule, BodyComponent],
+  imports: [RouterOutlet, NavbarComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  // title = 'first-app';
+  title = 'first-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ServicesComponent } from './components/services/services.component';
+import { HomeComponent } from './components/home/home.component';
+import { IndustriesComponent } from './components/industries/industries.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'services', component: ServicesComponent},
+    {path: 'industries', component: IndustriesComponent},
+];
