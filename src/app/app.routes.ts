3416f91ca6b82c7f69e254b17d7a8bfa8eb89f61
Mo-Zoka <zoka1080p@gmail.com>
2025-01-27 14:54:11 +0200

    login logic added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ServicesComponent } from './components/services/services.component';
 import { HomeComponent } from './components/home/home.component';
 import { IndustriesComponent } from './components/industries/industries.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'services', component: ServicesComponent},
     {path: 'industries', component: IndustriesComponent},
+    {path: 'login', component: LoginComponent},
 ];
